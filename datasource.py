6a988002385a57b367abba19fab31e650b78ce79
Joshua Rospondek <git@spondy.net>
2018-02-16 14:28:06 +0800

    Input target datetime

--- datasource.py
+++ datasource.py
@@ -1,17 +1,20 @@
 from ecusisParser import extract_time_slots
 from bs4 import BeautifulSoup
-import re
+from ecusisUtils import is_target_in_week, is_target_on_calendar, dmy_to_ecusisdate, ecusisdate_to_dmy
+import datetime
 
 class EcusisSource:
+    current_date = datetime.date.today()
     login_url = 'https://ecusis.ecu.edu.au/ECU/login_secure.aspx'
     timetable_url = 'https://ecusis.ecu.edu.au/roomBookings/timetable.aspx?loc_code=200012227'
     headers = { "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.84 Safari/537.36" }
     # we should in future remove uneccesary keys from formKeys, such as weekDate
-    formKeys = [ 'fromInterval', 'toInterval', 'userName', 'weekDate', '__EVENTTARGET',
-        '__EVENTARGUMENT', '__LASTFOCUS', '__VIEWSTATE', '__VIEWSTATEGENERATOR',
-        '__EVENTVALIDATION', 'pageWidth', 'txtMeetingTitle', 'selRecurInterval',
-        'recurDailySpacing', 'recurWeeklySpacing', 'listToDate', 'listToMonth',
-        'listToYear', 'listMonth', 'listYear']
+    #formKeys = [ 'fromInterval', 'toInterval', 'userName', 'weekDate', '__EVENTTARGET',
+    #    '__EVENTARGUMENT', '__LASTFOCUS', '__VIEWSTATE', '__VIEWSTATEGENERATOR',
+    #    '__EVENTVALIDATION', 'pageWidth', 'txtMeetingTitle', 'selRecurInterval',
+    #    'recurDailySpacing', 'recurWeeklySpacing', 'listToDate', 'listToMonth',
+    #    'listToYear', 'listMonth', 'listYear']
+    formKeys = ['__VIEWSTATE', '__VIEWSTATEGENERATOR', '__EVENTVALIDATION']
 
     def __login(self):
         f = open('credentials\password.txt', 'r')
@@ -36,18 +39,178 @@ class EcusisSource:
             return result.get('value', '')
         return ''
 
-<<<<<<< HEAD
-    def GetHtml(self):
-        ######### initial request to page #########
-        # we need to make this before we can make a request date, for some reason.
-=======
+    def testclass(self):
+        payload = {'pageWidth' : '1200'}
+
+        timetable_url = 'https://ecusis.ecu.edu.au/roomBookings/timetable.aspx?loc_code=200012227'
+
+        r = self.session.post(timetable_url, data = payload, headers = self.headers)
+        f = open('output/out227.html', 'w')
+        f.write("".join(map(chr, r.content)))
+        f.close()
+        # construct the next post request using the viewstate etc from that page
+        soup = BeautifulSoup(r.content, 'html.parser')
+        payload = {}
+        for key in self.formKeys:
+            payload[key] = self.__getvalue(key, soup)
+
+
+        payload['__EVENTTARGET'] = 'listMonth'
+        payload['__EVENTARGUMENT'] = ''
+        payload['listMonth'] = 'April'
+        payload['listYear'] = '2018'
+        # these keys must be populated with valid values but do not need to match __EVENTARGUMENT
+        payload['selRecurInterval'] = 'Weekly'
+        payload['listToMonth'] = 'Jan'
+        payload['listToYear'] = '2018'
+
+        r = self.session.post(timetable_url, data = payload, headers = self.headers)
+        f = open('output/out227_apr.html', 'w')
+        f.write("".join(map(chr, r.content)))
+        f.close()
+        # construct the next post request using the viewstate etc from that page
+        soup = BeautifulSoup(r.content, 'html.parser')
+        payload = {}
+        for key in self.formKeys:
+            payload[key] = self.__getvalue(key, soup)
+
+        payload['__EVENTTARGET'] = 'calendar'
+        payload['__EVENTARGUMENT'] = '6694'
+
+        r = self.session.post(timetable_url, data = payload, headers = self.headers)
+        f = open('output/out227_apr30.html', 'w')
+        f.write("".join(map(chr, r.content)))
+        f.close()
+
+        timetable_url = 'https://ecusis.ecu.edu.au/roomBookings/timetable.aspx?loc_code=200012208'
+        r = self.session.post(timetable_url, data = payload, headers = self.headers)
+        f = open('output/out208_apr30.html', 'w')
+        f.write("".join(map(chr, r.content)))
+        f.close()
+
+    #def __select_date(self, target_date):
+    def testclass2(self, target_date):
+        calendar_check = is_target_on_calendar(self.current_date, target_date)
+        print('>>>> Checking target date')
+        print('>> Calendar check -', calendar_check)
+        week_check = is_target_in_week(self.current_date, target_date)
+        print('>> Week check -', week_check)
+
+        timetable_url = 'https://ecusis.ecu.edu.au/roomBookings/timetable.aspx?loc_code=200012227'
+        payload = {'pageWidth' : '1200'}
+
+        if calendar_check == False or week_check == False:
+            print('>> Date change required')
+            print('>> Initialising ...', end = '')
+            r = self.session.post(timetable_url, data = payload, headers = self.headers)
+            soup = BeautifulSoup(r.content, 'html.parser')
+            payload = {}
+            for key in self.formKeys:
+                payload[key] = self.__getvalue(key, soup)
+            print('DONE')
+
+        if calendar_check == False:
+            print('>> Changing calendar ...', end = '')
+            payload['__EVENTTARGET'] = 'listMonth'
+            payload['__EVENTARGUMENT'] = ''
+            payload['listMonth'] = target_date.strftime("%B")
+            payload['listYear'] = target_date.strftime("%Y")
+
+            # these keys must be populated with valid values but do not need to match __EVENTARGUMENT
+            payload['selRecurInterval'] = 'Weekly'
+            payload['listToMonth'] = 'Jan'
+            payload['listToYear'] = '2018'
+            print(payload['listMonth'], payload['listYear'], '...', end = '')
+
+            r = self.session.post(timetable_url, data = payload, headers = self.headers)
+
+            # construct the next post request using the viewstate etc from that page
+            soup = BeautifulSoup(r.content, 'html.parser')
+            payload = {}
+            for key in self.formKeys:
+                payload[key] = self.__getvalue(key, soup)
+
+            f = open('output/out_month.html', 'w')
+            f.write("".join(map(chr, r.content)))
+            f.close()
+
+            print('DONE')
+
+        if week_check == False:
+            print('>> Changing week ...', end = '')
+            target_date_ecusis = dmy_to_ecusisdate(target_date)
+            payload['__EVENTTARGET'] = 'calendar'
+            payload['__EVENTARGUMENT'] = str(target_date_ecusis)
+            print(payload['__EVENTARGUMENT'], '...', end = '')
+
+            r = self.session.post(timetable_url, data = payload, headers = self.headers)
+
+            # construct the next post request using the viewstate etc from that page
+            soup = BeautifulSoup(r.content, 'html.parser')
+            payload = {}
+            for key in self.formKeys:
+                payload[key] = self.__getvalue(key, soup)
+
+            f = open('output/out_week.html', 'w')
+            f.write("".join(map(chr, r.content)))
+            f.close()
+
+            print('DONE')
+
+        print('>> Check complete')
+
+    #def testclass2(self, target_date):
+
+        #self.__select_date(target_date)
+
+        print('>>>> Requesting timetables')
+        payload['pageWidth'] = '1200'
+
+        print('>> Requesting 227 ...', end = '')
+        timetable_url = 'https://ecusis.ecu.edu.au/roomBookings/timetable.aspx?loc_code=200012227'
+        r = self.session.post(timetable_url, data = payload, headers = self.headers)
+        f = open('output/out227.html', 'w')
+        f.write("".join(map(chr, r.content)))
+        f.close()
+        print('DONE')
+
+        print('>> Requesting 208 ...', end = '')
+        timetable_url = 'https://ecusis.ecu.edu.au/roomBookings/timetable.aspx?loc_code=200012208'
+        r = self.session.post(timetable_url, data = payload, headers = self.headers)
+        f = open('output/out208.html', 'w')
+        f.write("".join(map(chr, r.content)))
+        f.close()
+        print('DONE')
+
+        print('>> Requesting 219 ...', end = '')
+        timetable_url = 'https://ecusis.ecu.edu.au/roomBookings/timetable.aspx?loc_code=200012219'
+        r = self.session.post(timetable_url, data = payload, headers = self.headers)
+        f = open('output/out219.html', 'w')
+        f.write("".join(map(chr, r.content)))
+        f.close()
+        print('DONE')
+
+        print('>> Requesting 236 ...', end = '')
+        timetable_url = 'https://ecusis.ecu.edu.au/roomBookings/timetable.aspx?loc_code=200012236'
+        r = self.session.post(timetable_url, data = payload, headers = self.headers)
+        f = open('output/out236.html', 'w')
+        f.write("".join(map(chr, r.content)))
+        f.close()
+        print('DONE')
+
+        print('>> Requesting 236 ...', end = '')
+        timetable_url = 'https://ecusis.ecu.edu.au/roomBookings/timetable.aspx?loc_code=200011123'
+        r = self.session.post(timetable_url, data = payload, headers = self.headers)
+        f = open('output/out123.html', 'w')
+        f.write("".join(map(chr, r.content)))
+        f.close()
+        print('DONE')
+
     def get_time_slots(self):
         # initial request to page - we need to make this before we can make a request
         # date, for some reason.
->>>>>>> 415dcc33e6c32a74e4160deb51c1a9eff44a929f
         payload = {'pageWidth' : '1200'}
         r = self.session.post(self.timetable_url, data = payload, headers = self.headers)
-        print ('Initial request...', r.status_code)
 
         # construct the next post request using the viewstate etc from that page
         soup = BeautifulSoup(r.content, 'html.parser')
@@ -55,66 +218,24 @@ class EcusisSource:
         for key in self.formKeys:
             payload[key] = self.__getvalue(key, soup)
 
-        src = "".join(map(chr, r.content))
-        dates = re.findall(r"__doPostBack\('calendar','([0-9]+)'\)", src)
-        start_date = int(dates[0])
-        end_date = int(dates[-1])
-
-
-        # user input for desired date
-        user_date = 6609
-
-        if user_date > end_date:
-            ######### request to see next month's calendar #########
-            # select next month
-            payload['__EVENTTARGET'] = 'cmdNextMonth'
-            payload['__EVENTARGUMENT'] = ''
-            # these keys determine from which month you are moving from
-            # when you post cmdNextMonth or cmdPrevMonth
-            payload['listMonth'] = 'January'
-            payload['listYear'] = '2018'
-            # these keys must be populated with valid values but do not need to match the date
-            payload['selRecurInterval'] = 'Weekly'
-            payload['listToMonth'] = 'Mar'
-            payload['listToYear'] = '2019'
-            # weekDate is uneccesary and can be empty
-            #payload['weekDate'] = ''
-
-            r = self.session.post(self.timetable_url, data = payload, headers = self.headers)
-            print ('Next month........', r.status_code)
-
-            f = open('output/out0.html', 'w')
-            f.write("".join(map(chr, r.content)))
-            f.close()
-
-            ######### request to see specified date #########
-            # get new viewstate etc.
-            soup = BeautifulSoup(r.content, 'html.parser')
-            payload = {}
-            for key in self.formKeys:
-                payload[key] = self.__getvalue(key, soup)
-
-        # the date is an integer stored as a string in __EVENTARGUMENT
-        # where 01 Jan 2000 = 0
+        # the date is a integer stored as a string in __EVENTARGUMENT
+        # where 01 Jan 2000 = 0 (or 1, I'll do the maths later)
         #
         # The date must clickable on the calendar of the current date
         # any date outside of the range of the calendar will 404
         payload['__EVENTTARGET'] = 'calendar'
-        payload['__EVENTARGUMENT'] = str(user_date)
+        payload['__EVENTARGUMENT'] = '6610'
+
         # these keys must be populated with valid values but do not need to match __EVENTARGUMENT
         payload['selRecurInterval'] = 'Weekly'
         payload['listToMonth'] = 'Mar'
         payload['listToYear'] = '2019'
         payload['listMonth'] = 'March'
         payload['listYear'] = '2019'
+
         # weekDate is uneccesary and can be empty
         #payload['weekDate'] = ''
 
         r = self.session.post(self.timetable_url, data = payload, headers = self.headers)
-<<<<<<< HEAD
-        print ('Specific date.....', r.status_code)
-        return "".join(map(chr, r.content))
-=======
         html_str = "".join(map(chr, r.content))
         return extract_time_slots(html_str)
->>>>>>> 415dcc33e6c32a74e4160deb51c1a9eff44a929f
--- main.py
+++ main.py
@@ -1,10 +1,21 @@
 #!/bin/python3
 
 import requests
+import datetime
 from datasource import EcusisSource
 
-source = EcusisSource(requests.Session())
-time_slots = source.get_time_slots();
+#source = EcusisSource(requests.Session())
+#time_slots = source.get_time_slots()
 
-for t in time_slots:
-    print(t)
+#for t in time_slots:
+#    print(t)
+
+testdate = datetime.date.today()
+testdate = datetime.date(2018, 9, 9)
+print('Target date:', testdate)
+
+source = EcusisSource(requests.Session())
+#timetable = source.get_timetable()
+#source.testclass()
+source.testclass2(testdate)
+print("Winner winner")
